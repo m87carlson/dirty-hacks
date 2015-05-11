fc8121ed367694137b6326f37ebf0df3429a03c1
Mike Carlson <mike@bayphoto.com>
2015-05-11 15:47:52 -0700

    adding comments, so aliens can understand why I did this

--- rename_mugsy_data_dirs.rb
+++ rename_mugsy_data_dirs.rb
@@ -1,13 +1,25 @@
 require 'fileutils'
 
-mugsy_data_no_import_dir = "/home/DISCDRIVE/mikec/projects/mugsy_testing/000_NoImport_NoEmail"
+# Define the MugsyClicks manual upload directory is
+mugsy_data_dir = "/data/mugsyclicks/000_NoImport_NoEmail"
 
-Dir.glob("#{mugsy_data_no_import_dir}/*").each do |dir| 
+# Match all sub-directories, process each one.
+Dir.glob("#{mugsy_data_dir}/*").each do |dir| 
+
+  # If the directory has a known bad character:
+  #  * Whitespace
+  #  * $
+  #  * !
+  #  * :
+  # Then strip it out 
   if dir.to_str.match(/[\s|\\|:|$|!]/)
     fixed_dir = dir.to_str.gsub(/[\s|\\|:|$|!]/, '')
     puts "bad dir = #{fixed_dir}"
     puts "Renaming..."
+    # Move the directory from the old name,
+    # to the new name.
     FileUtils.mv dir.to_str, fixed_dir
   end
 
 end
+
