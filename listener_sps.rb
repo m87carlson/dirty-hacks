240ae8be87f7a79103d99fb5e6be2b1da85699ae
Mike Carlson <mike@bayphoto.com>
2015-12-22 15:15:27 -0800

    pre and post md5 sums work now, puts to the terminal for debug but otherwise logs moves and sums to a file

--- listener_sps.rb
+++ listener_sps.rb
@@ -19,32 +19,44 @@ listener = Listen.to('BAYXML-1') do |modified, added, removed|
     if tmp_item.to_i % 4 == 0
       begin
         FileUtils.mv(item, 'BAYXML-1/queue-4/')
-        logger.info('move') { "moved #{item} to BAYXML-1/queue-4/"}
-        logger.info('md5') { "MD5=#{pre_sum}" }
+        destination_path = File.join("BAYXML-1/queue-4", File.basename(item))
+        post_sum = Digest::MD5.file(destination_path)
+        puts "#{item} => #{destination_path}"
+        logger.info('move') { "moved #{item} => BAYXML-1/queue-4/"}
+        logger.info('md5') { "pre_md5=#{pre_sum} post_md5=#{post_sum}" }
       rescue
         logger.error "#{item} move failed"
       end
     elsif tmp_item.to_i % 3 == 0
       begin
         FileUtils.mv(item, 'BAYXML-1/queue-3/')
-        logger.info('move') { "moved #{item} to BAYXML-1/queue-3/"}
-        logger.info('md5') { "MD5=#{pre_sum}" }
+        destination_path = File.join("BAYXML-1/queue-3", File.basename(item))
+        post_sum = Digest::MD5.file(destination_path)
+        puts "#{item} => #{destination_path}"
+        logger.info('move') { "moved #{item} => BAYXML-1/queue-3/"}
+        logger.info('md5') { "pre_md5=#{pre_sum} post_md5=#{post_sum}" }
       rescue
         logger.error "#{item} move failed"
       end
     elsif tmp_item.to_i % 2 == 0
       begin
         FileUtils.mv(item, 'BAYXML-1/queue-2/')
-        logger.info('move') { "moved #{item} to BAYXML-1/queue-2/"}
-        logger.info('md5') { "MD5=#{pre_sum}" }
+        destination_path = File.join("BAYXML-1/queue-2", File.basename(item))
+        post_sum = Digest::MD5.file(destination_path)
+        puts "#{item} => #{destination_path}"
+        logger.info('move') { "moved #{item} => BAYXML-1/queue-2/"}
+        logger.info('md5') { "pre_md5=#{pre_sum} post_md5=#{post_sum}" }
       rescue
         logger.error "#{item} move failed"
       end
     else
       begin
         FileUtils.mv(item, 'BAYXML-1/queue-1/')
-        logger.info('move') { "moved #{item} to BAYXML-1/queue-1/"}
-        logger.info('md5') { "MD5=#{pre_sum}" }
+        destination_path = File.join("BAYXML-1/queue-1", File.basename(item))
+        post_sum = Digest::MD5.file(destination_path)
+        puts "#{item} => #{destination_path}"
+        logger.info('move') { "moved #{item} => BAYXML-1/queue-1/"}
+        logger.info('md5') { "pre_md5=#{pre_sum} post_md5=#{post_sum}" }
       rescue
         logger.error "#{item} move failed"
       end
