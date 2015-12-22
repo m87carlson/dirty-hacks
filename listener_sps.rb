4e5de5bff1107e564491bf18c5fecd05d0cf3be2
Mike Carlson <mike@bayphoto.com>
2015-12-22 12:28:46 -0800

    adding pre-move md5 checksums and  begin/rescue blocks

--- listener_sps.rb
+++ listener_sps.rb
@@ -3,6 +3,7 @@
 require 'listen'
 require 'fileutils'
 require 'logger'
+require 'digest'
 
 logger = Logger.new('/tmp/listener_sps.log')
 
@@ -11,21 +12,42 @@ listener = Listen.to('BAYXML-1') do |modified, added, removed|
   modified.each do |item|
     tmp_item = item.split('SPS').last.split('.').first.gsub(/(.zip|.txt|_done)/, '')
 
-    puts "temp item: #{tmp_item}"
+    # calculate a checksum
+    pre_sum = Digest::MD5.file(item)
 
 
     if tmp_item.to_i % 4 == 0
-      FileUtils.mv(item, 'BAYXML-1/queue-4/')
-      logger.info('move') { "moved #{item} to BAYXML-1/queue-4/"}
+      begin
+        FileUtils.mv(item, 'BAYXML-1/queue-4/')
+        logger.info('move') { "moved #{item} to BAYXML-1/queue-4/"}
+        logger.info('md5') { "MD5=#{pre_sum}" }
+      rescue
+        logger.error "#{item} move failed"
+      end
     elsif tmp_item.to_i % 3 == 0
-      FileUtils.mv(item, 'BAYXML-1/queue-3/')
-      logger.info('move') { "moved #{item} to BAYXML-1/queue-3/"}
+      begin
+        FileUtils.mv(item, 'BAYXML-1/queue-3/')
+        logger.info('move') { "moved #{item} to BAYXML-1/queue-3/"}
+        logger.info('md5') { "MD5=#{pre_sum}" }
+      rescue
+        logger.error "#{item} move failed"
+      end
     elsif tmp_item.to_i % 2 == 0
-      FileUtils.mv(item, 'BAYXML-1/queue-2/')
-      logger.info('move') { "moved #{item} to BAYXML-1/queue-2/"}
+      begin
+        FileUtils.mv(item, 'BAYXML-1/queue-2/')
+        logger.info('move') { "moved #{item} to BAYXML-1/queue-2/"}
+        logger.info('md5') { "MD5=#{pre_sum}" }
+      rescue
+        logger.error "#{item} move failed"
+      end
     else
-      FileUtils.mv(item, 'BAYXML-1/queue-1/')
-      logger.info('move') { "moved #{item} to BAYXML-1/queue-1/"}
+      begin
+        FileUtils.mv(item, 'BAYXML-1/queue-1/')
+        logger.info('move') { "moved #{item} to BAYXML-1/queue-1/"}
+        logger.info('md5') { "MD5=#{pre_sum}" }
+      rescue
+        logger.error "#{item} move failed"
+      end
     end
   end
 end
