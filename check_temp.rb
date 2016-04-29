64bffa9293b250fce0b284721881e19482445522
Mike Carlson <mike@bayphoto.com>
2016-04-29 16:43:46 -0700

    tiny refactor

--- check_temp.rb
+++ check_temp.rb
@@ -1,8 +1,14 @@
 #!/usr/local/bin/ruby
 
-SENSOR1_TEMP=`snmpwalk -Os -c public -v 1 10.5.68.58 1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.1`.split(" ").last.chomp
-SENSOR1_HUMIDITY=`snmpwalk -Os -c public -v 1 10.5.68.58 1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.1`.split(" ").last.chomp
-SENSOR2_TEMP=`snmpwalk -Os -c public -v 1 10.5.68.58 1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.2`.split(" ").last.chomp
-SENSOR2_HUMIDITY=`snmpwalk -Os -c public -v 1 10.5.68.58 1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.2`.split(" ").last.chomp
+SENSOR_HOST="mc-weather02"
 
-puts "host=mc-weather2|sensor1_temp=#{SENSOR1_TEMP}|sendor1_humidity=#{SENSOR1_HUMIDITY}|sensor2_temp=#{SENSOR2_TEMP}|sensor2_humidity=#{SENSOR2_HUMIDITY}"
+def get_temperature(id)
+  `snmpwalk -Os -c public -v 1 #{SENSOR_HOST} #{id}`.split(" ").last.chomp
+end
+
+sensor1_temp=get_temperature "1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.1"
+sensor1_humidity=get_temperature "1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.1"
+sensor2_temp=get_temperature  "1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.2"
+sensor2_humidity=get_temperature "1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.2"
+
+puts "host=#{SENSOR_HOST}|sensor1_temp=#{sensor1_temp}|sendor1_humidity=#{sensor1_humidity}|sensor2_temp=#{sensor2_temp}|sensor2_humidity=#{sensor2_humidity}"
