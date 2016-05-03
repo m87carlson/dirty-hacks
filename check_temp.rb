859f0b0eee48f6b79e15088d256f1fbd3a746d29
Mike Carlson <mike@bayphoto.com>
2016-05-03 09:33:03 -0700

    check_temp update, get_temperature function takes oid, and id for sensor it. loops through

--- check_temp.rb
+++ check_temp.rb
@@ -2,13 +2,13 @@
 
 SENSOR_HOST="mc-weather02"
 
-def get_temperature(id)
-  `snmpwalk -Os -c public -v 1 #{SENSOR_HOST} #{id}`.split(" ").last.chomp
+def get_temperature(oid, id)
+  `/usr/local/bin/snmpwalk -Os -c public -v 1 #{SENSOR_HOST} #{oid}#{id}`.split(" ").last.chomp
 end
 
-sensor1_temp=get_temperature "1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.1"
-sensor1_humidity=get_temperature "1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.1"
-sensor2_temp=get_temperature  "1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.2"
-sensor2_humidity=get_temperature "1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.2"
 
-puts "host=#{SENSOR_HOST}|sensor1_temp=#{sensor1_temp}|sendor1_humidity=#{sensor1_humidity}|sensor2_temp=#{sensor2_temp}|sensor2_humidity=#{sensor2_humidity}"
+(1..2).each do |sensor|
+  temp=get_temperature("1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.",sensor.to_s )
+  humidity=get_temperature("1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.",sensor.to_s)
+  `logger "host=#{SENSOR_HOST}|sensor=#{sensor}|temp=#{temp}|humidity=#{humidity}"`
+end
