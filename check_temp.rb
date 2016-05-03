c828fe50a44afe18b0a83a24b40e7ade5bd30600
Mike Carlson <mike@bayphoto.com>
2016-05-03 09:36:09 -0700

    renamed get_temperature to get_snmp_data

--- check_temp.rb
+++ check_temp.rb
@@ -2,13 +2,13 @@
 
 SENSOR_HOST="mc-weather02"
 
-def get_temperature(oid, id)
+def get_snmp_data(oid, id)
   `/usr/local/bin/snmpwalk -Os -c public -v 1 #{SENSOR_HOST} #{oid}#{id}`.split(" ").last.chomp
 end
 
 
 (1..2).each do |sensor|
-  temp=get_temperature("1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.",sensor.to_s )
-  humidity=get_temperature("1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.",sensor.to_s)
+  temp=get_snmp_data("1.3.6.1.4.1.318.1.1.10.3.13.1.1.3.",sensor.to_s )
+  humidity=get_snmp_data("1.3.6.1.4.1.318.1.1.10.3.13.1.1.6.",sensor.to_s)
   `logger "host=#{SENSOR_HOST}|sensor=#{sensor}|temp=#{temp}|humidity=#{humidity}"`
 end
